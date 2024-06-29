e9d095b8218b86bff760dfb788bfc66d3e118871
tmihret <abc123tadael@gmail.com>
2024-06-29 19:28:20 -0400

    changed the increase button and added decrease button

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,6 +1,8 @@
 import React, { useContext } from 'react';
 import { TiDelete } from 'react-icons/ti';
+import { FaPlusCircle } from "react-icons/fa";
 import { AppContext } from '../context/AppContext';
+import { FaMinusCircle } from "react-icons/fa";
 const ExpenseItem = (props) => {
     const { dispatch,currency } = useContext(AppContext);
     const handleDeleteExpense = () => {
@@ -19,11 +21,22 @@ const ExpenseItem = (props) => {
             payload: expense
         });
     }
+    const decreaseAllocation = (name) => {
+        const expense = {
+            name: name,
+            cost: -10,
+        };
+        dispatch({
+            type: 'ADD_EXPENSE',
+            payload: expense
+        });
+    }
     return (
         <tr>
         <td>{props.name}</td>
         <td>{currency}{props.cost}</td>
-        <td><button onClick={event=> increaseAllocation(props.name)}>+</button></td>
+        <td><FaPlusCircle size='2em'  style={{color: 'green'}} onClick={event=> increaseAllocation(props.name)}></FaPlusCircle></td>
+        <td><FaMinusCircle size='2em'  style={{color: 'red'}} onClick={event=> decreaseAllocation(props.name)}></FaMinusCircle></td>
         <td><TiDelete size='1.5em' onClick={handleDeleteExpense}></TiDelete></td>
         </tr>
     );
